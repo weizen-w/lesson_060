6d17014310bd139f9852ba2b7af619fe14093657
Wladimir <weizen.w@gmx.de>
2023-06-16 14:57:34 +0300

    feat: add Task1 solution

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    List<Book> books = new ArrayList<>();
+    books.add(new Book("Автор3", "Название2", 150));
+    books.add(new Book("Автор2", "Название1", 120));
+    books.add(new Book("Автор1", "Название1", 115));
+    books.add(new Book("Автор2", "Название3", 150));
+    books.add(new Book("Автор1", "Название1", 200));
+    System.out.println(books);
+    books.sort((o1, o2) -> {
+      if (o1.getTitle().equals(o2.getTitle())) {
+        return o1.getAuthor().compareTo(o2.getAuthor());
+      }
+      return o1.getTitle().compareTo(o2.getTitle());
+    });
+    System.out.println(books);
   }
 }
